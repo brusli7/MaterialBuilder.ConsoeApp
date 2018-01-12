ab77d885f8dab2ffd8c413574c57fed40e3c9d08
DESKTOP-02K9R3L\Simon <simon.mackenzie1@gmail.com>
2018-01-12 14:21:38 +0000

    Initial commit

---
+++ Test/MaterialBuilders/Material.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.MaterialBuilders
+{
+    public abstract class Material
+    {
+        public Position Positions { get; set; }
+        public abstract void Output();
+    }
+}
---
+++ Test/MaterialBuilders/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test.MaterialBuilders
+{
+    public class Rectangle : Material
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public override void Output()
+        {
+            Console.WriteLine("Rectangle ({0},{1}) width={2} height={3}", Positions.PositionX, Positions.PositionY, Width, Height);
+        }
+
+        public Rectangle(Position positions, int width, int height)
+        {
+            Positions = positions;
+            Width = width;
+            Height = height;
+        }
+    }
+}
